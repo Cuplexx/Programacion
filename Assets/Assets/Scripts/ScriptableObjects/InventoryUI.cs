a5e5e23d4741f88e3d58f263c425fe9bf31b1340
Cuplexx <pol.oficial05@gmail.com>
2026-02-09 12:47:14 +0100

    awdd

--- Assets/Assets/Scripts/ScriptableObjects/InventoryUI.cs
+++ Assets/Assets/Scripts/ScriptableObjects/InventoryUI.cs
@@ -9,16 +9,27 @@ public class InventoryUI : MonoBehaviour
 
     public ItemInfo itemInfo;
 
-    void Start()
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.I))
-        {
-            CreateItem(itemInfo);
-        }
+        //Añadir la funcion CreateItem al callback del inventario cuando se añade un objeto
+        //Importante que la funcion reciba un ItemInfo como parametro, o se quejara :C
+        Inventory.Instance.onAddedItem += CreateItem;
     }
 
     public void CreateItem(ItemInfo itemInfo)
     {
+        Transform slot = null;
+        //Buscar en todos los objetos hijos del layout(huevos)
+        for (int i = 0; 1 < itemLayout.childCount; i++)
+        {   
+            //Si el hueco no tiene objetos hijo, significa que está vacío
+            if(itemLayout.GetChild(i).childCount == 0)
+            {
+                //Se asigna el hueco vacío y se sale del bucle
+                slot = itemLayout.GetChild(i);
+                break;
+            }
+        }
         //Crear una nueva imagen y emparentarla al Layout para que lo ponga en su posición
         Image newItem = Instantiate(itemPrefab, itemLayout);
         //Cambiar el sprite de la imagen al icono del objeto
