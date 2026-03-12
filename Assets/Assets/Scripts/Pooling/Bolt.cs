df0c7e8fcf28b45ec2bb850a9be791f0c647a07f
Cuplexx <pol.oficial05@gmail.com>
2026-03-12 13:35:03 +0100

    awd

---
+++ Assets/Assets/Scripts/Pooling/Bolt.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Bolt : MonoBehaviour
+{
+    //El pool al que pertenece este objeto
+    public ObjectPool<Bolt> pool;
+   
+}
---
+++ Assets/Assets/Scripts/Pooling/PlayerShoot.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Sistema para porder usar el sistema de Pooling de Unity
+using UnityEngine.Pool;
+
+public class PlayerShoot : MonoBehaviour
+{
+    //El prefab que utilizara para crear los objetos del pool
+    [SerializeField] private Bolt boltPrefab;
+
+    [SerializeField] private Transform shootOrigin;
+    //El pool de proyectiles
+    private ObjectPool<Bolt> boltPool;
+
+    void Start()
+    {
+        boltPool = new ObjectPool<Bolt>(CreateBolt, GetBolt);
+    }
+
+    //Esta funcion se llama al crear el pool tantas veces como objetos pueda tener
+    //Por ejemplo, se se especifica un tamaño de 20 para el pool, llama a la funcion 20 veces
+
+    private Bolt CreateBolt()
+    {
+        //Crear un nuevo proyectil
+        Bolt bolt = Instantiate(boltPrefab);
+        //Asignar el pool del proyectil
+        bolt.pool = boltPool;
+        //Desactivar el proyectil para que esté oculto
+        bolt.gameObject.SetActive(false);
+        return bolt;
+    }
+
+    //Se llama cada vez que se coja un proyectil del pool
+    private void GetBolt(Bolt bolt)
+    {
+        //Al sacar un objeto del pool, lo principal es activarlo
+        bolt.gameObject.SetActive(true);
+        //Mover el proyecttil al punto de origen de disparo
+        bolt.transform.position = shootOrigin.position;
+        //Añadir fuerza al proyectil
+    }
+
+    //Se llama cada vez que un proyectil vuelva al pool
+    private void ReleaseBolt(Bolt bolt)
+    {
+        gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            //Coger un proyectil de los que haya en el pool
+            boltPool.Get();
+        }
+    }
+}
